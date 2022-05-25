8a1a075d7b7b65da628a8107f25ea07da772f67d
Ferdinan Jensen <fje@trifork.com>
2022-05-25 14:46:11 +0200

    added test-project and unit-tests for indiciesBuilder

---
+++ Cheetah.WebApi.Shared-test/models/IndexType.cs
@@ -0,0 +1,17 @@
+﻿using Cheetah.WebApi.Shared.Infrastructure.Services;
+
+namespace Cheetah.WebApi.Shared_test.models
+{
+    public class IndexType : IndexTypeBase
+    {
+
+        public static IndexType testIndex(string indexBase = "indexBase")
+        {
+            return new IndexType(indexBase);
+        }
+
+        public IndexType(string name) : base(name)
+        {
+        }
+    }
+}
