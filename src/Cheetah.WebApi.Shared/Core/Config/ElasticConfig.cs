d453dfd94b2502f5739e8dbac58b6e3b62b37a69
iblutrifork <iblu@trifork.com>
2022-10-06 13:51:19 +0200

    Fix warnings

--- src/Cheetah.WebApi.Shared/Core/Config/ElasticConfig.cs
+++ src/Cheetah.WebApi.Shared/Core/Config/ElasticConfig.cs
@@ -3,9 +3,9 @@
     public class ElasticConfig
     {
         public const string Position = "ElasticSearch";
-        public string IndexNamePrefix { get; set; }
-        public string Url { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string IndexNamePrefix { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
