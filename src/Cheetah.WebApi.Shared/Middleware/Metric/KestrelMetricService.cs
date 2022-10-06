9d07ad38150d972ba3e77c64ea44d95bd263e988
iblutrifork <iblu@trifork.com>
2022-10-06 14:19:21 +0200

    Remove obsolete imports

--- src/Cheetah.WebApi.Shared/Middleware/Metric/KestrelMetricService.cs
+++ src/Cheetah.WebApi.Shared/Middleware/Metric/KestrelMetricService.cs
@@ -1,18 +1,12 @@
 ﻿using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Prometheus;
-using System.Security.Cryptography.X509Certificates;
 
 namespace Cheetah.WebApi.Shared.Middleware.Metric
 {
     public class KestrelMetricService : BackgroundService
     {
-        KestrelMetricServer _kestrelserver; 
-        public KestrelMetricService(int port) 
+        KestrelMetricServer _kestrelserver;
+        public KestrelMetricService(int port)
         {
             _kestrelserver = new KestrelMetricServer(port);
         }
