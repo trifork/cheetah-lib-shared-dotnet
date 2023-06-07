46dee80b40e06dfd7621c7780b88f945892f0d11
Mikkel Bjerg <mbj@trifork.com>
2023-06-07 15:41:05 +0200

    Cleanup

--- src/Cheetah.ComponentTest.Test/Program.cs
+++
@@ -1,9 +0,0 @@
-using Cheetah.ComponentTest;
-using Serilog.Core;
-using Serilog.Events;
-
-await new ComponentTestRunner()
-    .AddAllTests()
-    .WithConfiguration<ComponentTestConfig>(ComponentTestConfig.Position)
-    .WithLoggingLevelSwitch(new LoggingLevelSwitch(LogEventLevel.Information))
-    .RunAsync(args);
