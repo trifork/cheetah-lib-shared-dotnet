01347c506c54e50b504be3c37f65f31b89839898
Magnus Nørgaard <mnr@trifork.com>
2023-07-14 12:32:08 +0200

    Change wording slightly

--- src/Cheetah.WebApi.Shared/Infrastructure/Auth/TimedOpenSearchTokenRefresherService.cs
+++ src/Cheetah.WebApi.Shared/Infrastructure/Auth/TimedOpenSearchTokenRefresherService.cs
@@ -70,7 +70,7 @@ namespace Cheetah.WebApi.Shared.Infrastructure.Auth
                     }
                     catch (OAuth2TokenException e)
                     {
-                        _logger.LogWarning($"Periodic refresh of OpenSearch Auth token failed with error: '{e.Error}'");
+                        _logger.LogWarning($"Failed to retrieve OpenSearch auth token with error: '{e.Error}'");
                     }
                 } while (await timer.WaitForNextTickAsync(cancellationToken));
             }
