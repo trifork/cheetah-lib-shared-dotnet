d483317c7da05080044034ede3b26fee21aaef7b
Casper Thygesen <cth@trifork.com>
2023-10-10 11:41:12 +0000

    reimplemented ServiceProvider

---
+++ src/Cheetah.WebApi.Shared/Infrastructure/ServiceProvider/IServiceCollectionInstaller.cs
@@ -0,0 +1,10 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Cheetah.WebApi.Shared.Infrastructure.ServiceProvider
+{
+    public interface IServiceCollectionInstaller
+    {
+        void Install(IServiceCollection services, IHostEnvironment hostEnvironment);
+    }
+}
