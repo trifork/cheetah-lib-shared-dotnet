bebc7c87e60e3eb30a4c2305754b76dfb6babe91
Kristian Kristensen <krkr@trifork.com>
2023-11-03 11:16:33 +0100

    made class for reading json file

--- src/Cheetah.ComponentTest/JSON/JsonReader.cs
+++ src/Cheetah.ComponentTest/JSON/JsonReader.cs
@@ -1,23 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace Cheetah.ComponentTest.JSON
 {
-
     public class JsonConfiguration
     {
-        public string ProducerTopic { get; set; }
-        public string ConsumerTopic { get; set; }
-
+        public string? ProducerTopic { get; set; }
+        public string? ConsumerTopic { get; set; }
+        public List<string>? ProducerData { get; set; }
+        public List<string>? ConsumerData { get; set; }
     }
 
     public class JsonReader
     {
+        const string FILE_PATH = "FOLDERPATH";
 
-        protected IConfiguration? Configuration { get; }
+        IConfiguration? Configuration { get; }
 
         public JsonReader(IConfiguration configuration)
         {
             Configuration = configuration;
+        }
+
+        protected JsonConfiguration ConsumeJson()
+        {
+            if (string.IsNullOrEmpty(FILE_PATH))
+            {
+                throw new InvalidDataException($"File path if not provided correct - {FILE_PATH}");
+            }
+            string filePath = Configuration.GetValue<string>(FILE_PATH);
+            JsonConfiguration? config;
+            if (!File.Exists(filePath))
+            {
+                throw new ArgumentException($"Can't find file: {filePath}");
+            }
+
+            string jsonFile = File.ReadAllText(filePath);
+            
+            try
+            {
+                string jsonText = File.ReadAllText(jsonFile);
+                config = JsonConvert.DeserializeObject<JsonConfiguration>(jsonText);
+
+                if (config == null)
+                {
+                    throw new Exception("Could not deserialize file");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error processing file {jsonFile}: {ex.Message}");
+            }
+            
+            return config;
         }
 
     }
