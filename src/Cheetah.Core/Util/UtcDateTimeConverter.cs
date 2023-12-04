1973b2435df4d86434311068b6becbc9e241561e
Magnus Nørgaard <mnr@trifork.com>
2023-12-04 13:37:09 +0100

    Make UtcDateTimeConverter able to return DateTimeOffset

--- src/Cheetah.Core/Util/UtcDateTimeConverter.cs
+++ src/Cheetah.Core/Util/UtcDateTimeConverter.cs
@@ -14,21 +14,22 @@ namespace Cheetah.Core.Util
         {
             try
             {
-                return reader.Value switch
+                if(objectType == typeof(DateTime))
                 {
-                    long val => DateTime.UnixEpoch.AddMilliseconds(val).ToUniversalTime(),
-                    string val => DateTime.TryParse(val, out var dateTime)
-                            ? dateTime.ToUniversalTime()
-                            : throw new ArgumentException(
-                                $"Attempted to deserialize the string '{val}', into a DateTime, but it could not be parsed"
-                            ),
-                    DateTime val => val.ToUniversalTime(),
-                    DateTimeOffset val => new DateTime(val.ToUniversalTime().Ticks, DateTimeKind.Utc),
-                    null => null,
-                    _ => throw new ArgumentException(
-                            $"Unable to deserialize type '{reader.Value?.GetType().FullName}' into a DateTime."
-                        )
-                };
+                    return ReadDateTime(reader, objectType, existingValue, serializer);
+                }
+
+                if (objectType == typeof(DateTimeOffset))
+                {
+                    var dateTime = ReadDateTime(reader, objectType, existingValue, serializer);
+                    return dateTime.HasValue
+                        ? new DateTimeOffset(dateTime.Value)
+                        : null;
+                }
+                
+                throw new ArgumentException(
+                    $"Cannot convert to requested object type: {objectType.FullName}. " +
+                    $"Supported types are {typeof(DateTime).FullName} and {typeof(DateTimeOffset).FullName}");
             }
             catch (Exception e)
             {
@@ -37,6 +38,26 @@ namespace Cheetah.Core.Util
                     e
                 );
             }
+        }
+
+        private static DateTime? ReadDateTime(JsonReader reader, Type objectType, object? existingValue,
+            JsonSerializer serializer)
+        {
+            return reader.Value switch
+            {
+                long val => DateTime.UnixEpoch.AddMilliseconds(val).ToUniversalTime(),
+                string val => DateTime.TryParse(val, out var dateTime)
+                    ? dateTime.ToUniversalTime()
+                    : throw new ArgumentException(
+                        $"Attempted to deserialize the string '{val}', into a DateTime, but it could not be parsed"
+                    ),
+                DateTime val => val.ToUniversalTime(),
+                DateTimeOffset val => new DateTime(val.ToUniversalTime().Ticks, DateTimeKind.Utc),
+                null => null,
+                _ => throw new ArgumentException(
+                    $"Unable to deserialize type '{reader.Value?.GetType().FullName}' into a DateTime."
+                )
+            };
         }
 
         public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
@@ -52,7 +73,10 @@ namespace Cheetah.Core.Util
                     dto == default
                         ? 0
                         : dto.ToUnixTimeMilliseconds(),
-                _ => throw new ArgumentOutOfRangeException(nameof(value), value, $"Unable to extract epoch millis from object of type {value?.GetType().Name}")
+                _ => throw new ArgumentOutOfRangeException(
+                    nameof(value), 
+                    value, 
+                    $"Unable to extract epoch millis from object of type {value.GetType().Name}")
             };
             writer.WriteValue(unixTimeMilliseconds);
         }
