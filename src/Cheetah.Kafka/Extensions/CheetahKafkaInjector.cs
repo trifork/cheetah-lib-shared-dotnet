533360d24ccc09df26212cca597512dcd35fc7f2
Magnus Nørgaard <mnr@trifork.com>
2024-01-02 09:38:43 +0100

    Xml docs

--- src/Cheetah.Kafka/Extensions/CheetahKafkaInjector.cs
+++ src/Cheetah.Kafka/Extensions/CheetahKafkaInjector.cs
@@ -4,6 +4,9 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Cheetah.Kafka.Extensions
 {
+    /// <summary>
+    /// Utility class used to inject Kafka clients into a service collection
+    /// </summary>
     public class CheetahKafkaInjector
     {
         private readonly IServiceCollection _serviceCollection;
@@ -12,29 +15,66 @@ namespace Cheetah.Kafka.Extensions
         {
             _serviceCollection = serviceCollection;
         }
+        
+        /// <summary>
+        /// Registers a pre-configured <see cref="IProducer{TKey,TValue}"/>/>
+        /// </summary>
+        /// <param name="configAction">Additional configuration that this specific producer should use</param>
+        /// <typeparam name="TKey">The type of key that the injected producer will produce</typeparam>
+        /// <typeparam name="TValue">The type of value that the injected producer will produce</typeparam>
+        /// <returns>This <see cref="CheetahKafkaInjector"/> instance for method chaining</returns>
         public CheetahKafkaInjector WithProducer<TKey, TValue>(Action<ProducerConfig>? configAction = null)
         {
             _serviceCollection.AddSingleton(provider => GetFactory(provider).CreateProducer<TKey, TValue>(configAction));
             return this;
         }
         
+        /// <summary>
+        /// Registers a pre-configured, keyed <see cref="IProducer{TKey,TValue}"/>/>
+        /// </summary>
+        /// <param name="key">The key that the producer should be registered with</param>
+        /// <param name="configAction">Additional configuration that this specific producer should use</param>
+        /// <typeparam name="TKey">The type of key that the injected producer will produce</typeparam>
+        /// <typeparam name="TValue">The type of value that the injected producer will produce</typeparam>
+        /// <returns>This <see cref="CheetahKafkaInjector"/> instance for method chaining</returns>
         public CheetahKafkaInjector WithKeyedProducer<TKey, TValue>(object? key, Action<ProducerConfig>? configAction = null)
         {
             _serviceCollection.AddKeyedSingleton(key, (provider, o) => GetFactory(provider).CreateProducer<TKey, TValue>(configAction) );
             return this;
         }
+        
+        /// <summary>
+        /// Registers a pre-configured <see cref="IConsumer{TKey,TValue}"/>/>
+        /// </summary>
+        /// <param name="configAction">Additional configuration that this specific consumer should use</param>
+        /// <typeparam name="TKey">The type of key that the injected consumer will consume</typeparam>
+        /// <typeparam name="TValue">The type of value that the injected consumer will consume</typeparam>
+        /// <returns>This <see cref="CheetahKafkaInjector"/> instance for method chaining</returns>
         public CheetahKafkaInjector WithConsumer<TKey, TValue>(Action<ConsumerConfig>? configAction = null)
         {
             _serviceCollection.AddSingleton(provider => GetFactory(provider).CreateConsumer<TKey, TValue>(configAction));
             return this;
         }
 
+        /// <summary>
+        /// Registers a pre-configured, keyed <see cref="IConsumer{TKey,TValue}"/>/>
+        /// </summary>
+        /// <param name="configAction">Additional configuration that this specific consumer should use</param>
+        /// <param name="key">The key that the consumer should be registered with</param>
+        /// <typeparam name="TKey">The type of key that the injected consumer will consume</typeparam>
+        /// <typeparam name="TValue">The type of value that the injected consumer will consume</typeparam>
+        /// <returns>This <see cref="CheetahKafkaInjector"/> instance for method chaining</returns>
         public CheetahKafkaInjector WithKeyedConsumer<TKey, TValue>(object? key, Action<ConsumerConfig>? configAction = null)
         {
             _serviceCollection.AddKeyedSingleton(key, (provider, o) => GetFactory(provider).CreateConsumer<TKey, TValue>(configAction));
             return this;
         }
 
+        /// <summary>
+        /// Registers a pre-configured <see cref="IAdminClient"/>/>
+        /// </summary>
+        /// <param name="configAction">Additional configuration that this specific admin client should use</param>
+        /// <returns></returns>
         public CheetahKafkaInjector WithAdminClient(Action<AdminClientConfig>? configAction = null)
         {
             _serviceCollection.AddSingleton(provider => GetFactory(provider).CreateAdminClient(configAction));
--- src/Cheetah.OpenSearch/OpenSearchClientOptions.cs
+++ src/Cheetah.OpenSearch/OpenSearchClientOptions.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using Newtonsoft.Json;
+using OpenSearch.Client;
 
 namespace Cheetah.OpenSearch
 {
     public class OpenSearchClientOptions
     {
-        public JsonSerializerSettings JsonSerializerSettings { get; private set; } = new JsonSerializerSettings()
+        /// <summary>
+        /// Retrieves the current <see cref="JsonSerializerSettings"/> used by the <see cref="OpenSearchClient"/>
+        /// </summary>
+        public JsonSerializerSettings JsonSerializerSettings { get; } = new JsonSerializerSettings()
         {
             MissingMemberHandling = MissingMemberHandling.Ignore
         };
         
+        /// <summary>
+        /// Gets or sets a value indicating whether direct streaming of the response content should be disabled.<br/>
+        /// This should <c>false</c> in production environments to avoid buffering of the response content in memory.<br/>
+        /// When set to <c>true</c> the response content will be available during debugging, which might not otherwise be the case.
+        /// </summary>
         public bool DisableDirectStreaming { get; set; }
 
+        /// <summary>
+        /// Configures the <see cref="JsonSerializerSettings"/> used by the <see cref="OpenSearchClient"/>
+        /// </summary>
+        /// <param name="configure">Action which configures used <see cref="JsonSerializerSettings"/></param>
+        /// <returns>This <see cref="OpenSearchClientOptions"/> instance for method chaining.</returns>
         public OpenSearchClientOptions WithJsonSerializerSettings(Action<JsonSerializerSettings> configure)
         {
             configure(JsonSerializerSettings);
