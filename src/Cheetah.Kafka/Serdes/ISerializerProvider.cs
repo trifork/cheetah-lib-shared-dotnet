0ffa920803f8db38be080681a7d2e27eb049ce9b
pegtrifork <peg@trifork.com>
2024-06-10 15:41:58 +0200

    Rename default (de)serializer and added some documentation

--- src/Cheetah.Kafka/Serdes/ISerializerProvider.cs
+++ src/Cheetah.Kafka/Serdes/ISerializerProvider.cs
@@ -3,22 +3,22 @@
 namespace Cheetah.Kafka.Serdes
 {
     /// <summary>
-    /// 
+    /// Interface for serializer provider to be used to serialize Kafka Messages
     /// </summary>
     public interface ISerializerProvider
     {
         /// <summary>
-        /// 
+        /// Get a value serializer
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
+        /// <returns><see cref="ISerializer{T}"/></returns>
         ISerializer<T> GetValueSerializer<T>();
 
         /// <summary>
-        /// 
+        /// Get a key serializer
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
+        /// <returns><see cref="ISerializer{T}"/></returns>
         ISerializer<T>? GetKeySerializer<T>();
     }
 }
