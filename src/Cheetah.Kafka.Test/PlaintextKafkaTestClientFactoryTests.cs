b2765e1cf96c04c740d4325c5792ffa9e0c18cb0
rla <rla@trifork.com>
2025-06-19 10:05:08 +0200

    increase timeout in flakey test

--- src/Cheetah.Kafka.Test/PlaintextKafkaTestClientFactoryTests.cs
+++ src/Cheetah.Kafka.Test/PlaintextKafkaTestClientFactoryTests.cs
@@ -84,7 +84,7 @@ namespace Cheetah.Kafka.Test
             };
 
             await writer.WriteAsync(message);
-            var readMessages = reader.ReadMessages(1, TimeSpan.FromSeconds(5));
+            var readMessages = reader.ReadMessages(1, TimeSpan.FromSeconds(10));
             readMessages.Should().HaveCount(1);
             reader.VerifyNoMoreMessages(TimeSpan.FromSeconds(1)).Should().BeTrue();
         }
