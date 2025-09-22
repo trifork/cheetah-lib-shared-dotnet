d20df641aa2ffa9af1b25c44da914ae271a53a6d
Dolan <chdo@trifork.com>
2025-09-22 10:17:26 +0200

    changes configuration in AvroKafkaTestClientFactory to match PlaintextKafkaIntegrationTests

--- src/Cheetah.SchemaRegistry.Test/AvroKafkaTestClientFactoryTests.cs
+++ src/Cheetah.SchemaRegistry.Test/AvroKafkaTestClientFactoryTests.cs
@@ -22,18 +22,8 @@ namespace Cheetah.SchemaRegistry.Test
         {
             var localConfig = new Dictionary<string, string?>
             {
-                { "KAFKA:URL", "localhost:9092" },
-                { "KAFKA:OAUTH2:CLIENTID", "default-access" },
-                { "KAFKA:OAUTH2:CLIENTSECRET", "default-access-secret" },
-                { "KAFKA:OAUTH2:TOKENENDPOINT", "http://localhost:1852/realms/local-development/protocol/openid-connect/token " },
-                { "KAFKA:OAUTH2:SCOPE", "kafka" },
-                { "KAFKA:SECURITYPROTOCOL", "SaslPlaintext" },
-                { "KAFKA:SASLMECHANISM", "OAuthBearer" },
-                { "SCHEMAREGISTRY:URL", "http://localhost:8081/apis/ccompat/v7" },
-                { "SCHEMAREGISTRY:OAUTH2:CLIENTID", "default-access" },
-                { "SCHEMAREGISTRY:OAUTH2:CLIENTSECRET", "default-access-secret" },
-                { "SCHEMAREGISTRY:OAUTH2:TOKENENDPOINT", "http://localhost:1852/realms/local-development/protocol/openid-connect/token " },
-                { "SCHEMAREGISTRY:OAUTH2:SCOPE", "schema-registry" },
+                { "KAFKA:URL", "localhost:9093" },
+                { "KAFKA:SECURITYPROTOCOL", "Plaintext" },
             };
 
             var configuration = new ConfigurationBuilder()
@@ -99,7 +89,6 @@ namespace Cheetah.SchemaRegistry.Test
 
             // Act
             await writerAvro.WriteAsync(message);
-            await Task.Delay(1000);
             var readMessages = readerAvro.ReadMessages(1, TimeSpan.FromSeconds(5));
 
             // Assert
